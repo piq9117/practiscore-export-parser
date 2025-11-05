ec5613c69a56a1978dc6e85cf109f33c4860aa07
piq9117 <piq9117@gmail.com>
2025-11-05 15:38:07 -0500

    initial stage parser

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,13 @@
 module Spec (main) where
 
 import Spec.Parser.Shooter qualified
+import Spec.Parser.Stage qualified
 import Test.Tasty (defaultMain, testGroup)
 
 main :: IO ()
 main =
   (defaultMain <<< testGroup "Parser Spec")
     =<< sequence
-      [ Spec.Parser.Shooter.testTree
+      [ Spec.Parser.Shooter.testTree,
+        Spec.Parser.Stage.testTree
       ]
---
+++ test/Spec/Parser/Stage.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Spec.Parser.Stage where
+
+import Practiscore.Parser.Stage (stagesWithFieldName)
+import Test.Hspec (Spec, describe, it, shouldBe)
+import Test.Tasty (TestTree)
+import Test.Tasty.Hspec (testSpec)
+import Text.Megaparsec (runParser)
+
+stageParserSpec :: Spec
+stageParserSpec = do
+  describe "StageParser" $ do
+    let header :: Text = "F Number,Guntype,Minimum Rounds,Maximum Points,Classifier,Classifier_No,Stage_name,ScoringType,TimesRun"
+    let stageLineData :: Text = "G 1,Pistol,28,140,No,,I Am A Meat Popsicle,Comstock,1"
+
+    it "stagesWithFieldName" $
+      shouldBe
+        (runParser stagesWithFieldName mempty (header <> "\n" <> stageLineData <> "\n"))
+        ( Right
+            [ [ ("Number", "1"),
+                ("Guntype", "Pistol"),
+                ("Minimum Rounds", "28"),
+                ("Maximum Points", "140"),
+                ("Classifier", "No"),
+                ("Classifier_No", ""),
+                ("Stage_name", "I Am A Meat Popsicle"),
+                ("ScoringType", "Comstock"),
+                ("TimesRun", "1")
+              ]
+            ]
+        )
+
+testTree :: IO TestTree
+testTree = testSpec "Parser.Stage" $ do
+  stageParserSpec
